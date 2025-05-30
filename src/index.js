6ba1ee0ed035141e60ccd63bb05d0d282e8a0b6e
Abid Hussain <abidhussaingithub@gmail.com>
2025-05-30 11:22:59 +0500

    MongoDB added to the project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+import mongoose from 'mongoose';
 import app from './app.js';
 import logger from './configs/logger.js';
 
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
 
-console.log(process.env.NODE_ENV);
+// Handle MongoDB connection errors
+mongoose.connection.on('error', (err) => {
+  logger.error('MongoDB connection error:', err);
+  process.exit(1); // Exit the process if MongoDB connection fails
+});
+
+// Connect to MongoDB 
+mongoose.connect(DATABASE_URL ) 
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  });
+ 
+// mongodb debugging mod
+if (process.env.NODE_ENV !== 'production') {
+  mongoose.set('debug', true);
+} 
+
 
 let server;
 server = app.listen(PORT, () => {
